08ddd807a7e66d4a5ee2d82a84d62385d3d36ba6
Andranik <andran1401@gmail.com>
2023-03-18 18:16:35 +0400

    Exercise01

---
+++ task01/Program.cs
@@ -0,0 +1,26 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+
+double SumNum(int a , int b)
+{
+
+    double n_num = 1;
+    int b_abs = Math.Abs(b);
+
+    for (int i = 1; i <= b_abs; i++)
+    {
+
+        if (b > 0)
+            n_num *= a;
+        else
+            n_num /= a;
+    }
+
+    return n_num;
+}
+
+Console.WriteLine("enter number ");
+int num_1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("enter degree ");
+int num_2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine(SumNum(num_1, num_2));
