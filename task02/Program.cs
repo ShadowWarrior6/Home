4c57b4f92ec66731af52b35f0cdc2bf2958e7d3a
Andranik <andran1401@gmail.com>
2023-03-18 18:19:37 +0400

    Exercise02

---
+++ task02/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе
+
+Console.WriteLine("Введите число");
+
+int AnyNum(int num)
+{
+    int n_Sum = 0;
+
+    while (num > 0)
+    {
+
+        n_Sum += num % 10;
+        num /= 10;
+
+    }
+    return n_Sum;
+
+}
+
+int num_1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("как-то так " + AnyNum(num_1));
+
+
